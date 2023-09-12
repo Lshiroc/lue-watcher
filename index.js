90ab54fd3069dfec99361b46d94fdce8cea902e5
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-09-13 00:00:00 +0400

    deleted mongodb related lines

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const PORT = 8000;
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', (err) => console.error(err));
 app.use(express.json());
 app.use(cors({
   origin: 'http://127.0.0.1:8000'
