e4a116ba0e6123c6927e61fe9b165c6a53ffe5de
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-09-17 09:09:09 +0400

    fully migrated to typescript

--- server/routes/script.js
+++ server/routes/script.js
@@ -2,8 +2,38 @@ const winWidth = window.innerWidth;
 const winHeight = window.innerHeight;
 let arr = [];
 document.addEventListener('mouseover', (e) => {
+    var _a, _b;
     const elTarget = e.target;
     const xp = xpath(elTarget);
+    if (arr.length > 0) {
+        if ((_a = evaluateXPath(xp)) === null || _a === void 0 ? void 0 : _a.contains(evaluateXPath(arr[arr.length - 1].xpath))) {
+            console.log("stopped", arr);
+            return false;
+        }
+        if ((_b = evaluateXPath(arr[arr.length - 1].xpath)) === null || _b === void 0 ? void 0 : _b.contains(evaluateXPath(xp))) {
+            console.log("last", arr[arr.length - 1].xpath);
+            arr.pop();
+        }
+    }
+    let elementData = elTarget.getBoundingClientRect();
+    let newData = {
+        xpath: xp,
+        width: elementData.width,
+        height: elementData.height,
+        left: elementData.left,
+        top: elementData.top,
+        mouseX: (e.clientX / winWidth) * 100,
+        mouseY: (e.clientY / winHeight) * 100,
+        winWidth,
+        winHeight,
+        isLocationCentered: elementData.width < 200 || elementData.height < 200
+    };
+    arr.push(newData);
+    console.log("added", arr);
+    // Convert XPath to Node 
+    function evaluateXPath(xpath) {
+        return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
 });
 function xpath(el) {
     var _a;
@@ -14,6 +44,10 @@ function xpath(el) {
     if (el.id)
         return "//*[@id='" + el.id + "']";
     var sames = [].filter.call((_a = el.parentNode) === null || _a === void 0 ? void 0 : _a.children, function (x) { return x.tagName == el.tagName; });
-    return xpath(el.parentNode) + '/' + el.tagName.toLowerCase() + (sames.length > 1 ? '[' + ([].indexOf.call(sames, el) + 1) + ']' : '');
+    return xpath(el.parentElement) + '/' + el.tagName.toLowerCase() + (sames.length > 1 ? '[' + ([].indexOf.call(sames, el) + 1) + ']' : '');
 }
+function info() {
+    console.log(JSON.stringify(arr));
+}
+window.info = info;
 export {};
--- server/routes/script.ts
+++ server/routes/script.ts
@@ -1,5 +1,5 @@
 type Pattern =  {
-    xpath: string,
+    xpath: string|XPathResult,
     width: number,
     height: number,
     left: number,
@@ -15,15 +15,60 @@ const winWidth: number = window.innerWidth;
 const winHeight: number = window.innerHeight;
 let arr: Pattern[] = [];
 
-document.addEventListener('mouseover', (e: MouseEvent): void => {
-    const elTarget = e.target as Node;
+document.addEventListener('mouseover', (e: MouseEvent): boolean|void => {
+    const elTarget = e.target as HTMLElement;
     const xp = xpath(elTarget);
+
+    if(arr.length > 0) {
+        if(evaluateXPath(xp as string)?.contains(evaluateXPath(arr[arr.length - 1].xpath))) {
+            console.log("stopped", arr);
+            return false;
+        }
+        if(evaluateXPath(arr[arr.length - 1].xpath)?.contains(evaluateXPath(xp))) {
+            console.log("last", arr[arr.length - 1].xpath);
+            arr.pop();
+        }
+    }
+
+    let elementData = elTarget.getBoundingClientRect();
+    let newData: Pattern = {
+        xpath: xp,
+        width: elementData.width,
+        height: elementData.height,
+        left: elementData.left,
+        top: elementData.top,
+        mouseX: (e.clientX/winWidth)*100,
+        mouseY: (e.clientY/winHeight)*100,
+        winWidth,
+        winHeight,
+        isLocationCentered: elementData.width < 200 || elementData.height < 200
+    }
+
+    arr.push(newData);
+    console.log("added", arr);
+
+    // Convert XPath to Node 
+    function evaluateXPath(xpath: string|XPathResult): (Node|null) {
+        return document.evaluate(
+            xpath as string,
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+        ).singleNodeValue;
+    }
 })
 
-function xpath(el: Element): (string|XPathResult) {
+function xpath(el: HTMLElement|null): (string|XPathResult) {
     if (typeof el == "string") return document.evaluate(el, document, null, 0, null);
     if (!el || el.nodeType != 1) return '';
     if (el.id) return "//*[@id='" + el.id + "']";
     var sames = [].filter.call(el.parentNode?.children, function (x: HTMLElement) { return x.tagName == el.tagName });
-    return xpath(el.parentNode) + '/' + el.tagName.toLowerCase() + (sames.length > 1 ? '['+([].indexOf.call(sames, el)+1)+']' : '');
+    return xpath(el.parentElement) + '/' + el.tagName.toLowerCase() + (sames.length > 1 ? '['+([].indexOf.call(sames, el as never)+1)+']' : '');
 }
+
+function info() {
+    console.log(JSON.stringify(arr));
+}
+
+(window as any).info = info;
