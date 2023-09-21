98ca00f1b150256b7e10662f5fd905874b63fd57
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-09-21 23:51:48 +0400

    fix try

--- server/routes/test.ts
+++ server/routes/test.ts
@@ -8,9 +8,10 @@ router.get('/', (req: Request, res: Response) => {
     try {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
-        const script = fs.readFileSync(`${__dirname}\\script.js`, 'utf-8');
-        res.header('Content-Type', 'text/javascript');
-        res.status(200).send(script);
+        // const script = fs.readFileSync(`${__dirname}/script.js`, 'utf-8');
+        // res.header('Content-Type', 'text/javascript');
+        // res.status(200).send(script);
+        res.status(200).send({path: __dirname});
     } catch(err: any) {
         res.status(500).json(err.message);
     }
